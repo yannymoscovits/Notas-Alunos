eb5c8f7fcda92c7e14c4328f230b3ae17e97decd
Yanny Dórea Moscovits <49878332+yannymoscovits@users.noreply.github.com>
2022-03-22 18:26:42 -0300

    Adicionar arquivos de projeto.

---
+++ Alunos/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alunos
+{
+    internal class Aluno
+    {
+
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public string Resultado()
+        {
+            
+            if (NotaFinal() >= 60)
+            {
+                return "Aprovado!";
+            }
+            else
+            {
+                return " Reprovado! Faltaram " + FaltamPontos() + " Pontos";
+                
+            }
+        }
+        public double FaltamPontos()
+        {
+            return 60 - NotaFinal();
+        }
+    }
+}
---
+++ Alunos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Alunos
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Aluno a1 = new Aluno();
+
+            Console.Write("Insira o Nome do Aluno: ");
+            a1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a três notas do aluno: ");
+            a1.Nota1 = double.Parse(Console.ReadLine());
+            a1.Nota2 = double.Parse(Console.ReadLine());
+            a1.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Notal Final = " + a1.NotaFinal().ToString("F2"));
+            Console.WriteLine(a1.Resultado());
+
+            
+
+
+        }
+    }
+
+}
